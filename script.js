80f98f214efea3c0d794eb4179947a3b5da77770
Ayush257S <ayushsrivastva2103@gmail.com>
2025-03-27 20:47:00 +0530

    add files

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ let compScore=0;
 
 const choices=document.querySelectorAll(".choice");
 
+const getCompchoice=()=>{
+    const options=["rock","paper","scissors"];
+    const randIdx=Math.floor(Math.random()*3); 
+    return options[randIdx];
+}
+
+const playGame=(userChoice)=>{
+    console.log("user choice =",userChoice);
+    //Generate computer choice
+    const compchoice=getCompchoice();
+    console.log("Computer choice =",compchoice);
+}
+
+
+
 choices.forEach((choice)=>{
-    choice.addEventListern("click",()=>{
+    choice.addEventListener("click",()=>{
         const userChoice=choice.getAttribute("id");
-        console.log("choice wad clicked",userChoice);
+        console.log(userChoice);
+        playGame(userChoice);
     })
 })
